9300e252150976c54d47eb0dc6401d0fac6b0b0e
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2020-06-04 20:02:55 -0300

    Criando classe Jogada.

---
+++ lib/Jogada.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Jogada extends StatefulWidget {
+  @override
+  _JogadaState createState() => _JogadaState();
+}
+
+class _JogadaState extends State<Jogada> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
